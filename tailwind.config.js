5a5b0a9ba67f05bfbe00f9d09e195b792af89e0e
Samuli Tamminen <samuli.tamminen@firstbeat.fi>
2023-01-24 21:40:10 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
