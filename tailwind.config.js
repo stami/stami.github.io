9c5804ea6c878398e4a66624000cfe6dec958273
Samuli Tamminen <samuli.tamminen@firstbeat.fi>
2023-01-28 15:15:43 +0200

    add a privacy policy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/index.html"],
+  content: ["./public/**/*"],
   theme: {
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
