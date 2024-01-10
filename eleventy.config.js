1c98ae3d556b7067655befbac9ed0ba12f71ae74
Samuli Tamminen <samuli.tamminen@firstbeat.fi>
2024-01-10 19:49:39 +0200

    change date formatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -47,7 +47,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
     // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
     return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
-      format || "dd LLLL yyyy"
+      format || "d LLLL yyyy"
     );
   });
 
